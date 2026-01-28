b21b3db808b441d19a8a9e9c7da250ea5e5b25a6
Shakir Ali <sayedshakirali0@gmail.com>
2026-01-28 20:00:33 +0530

    Fixing Queries

--- backend/src/main/java/com/shakirali/rental/repository/TenantRepository.java
+++ backend/src/main/java/com/shakirali/rental/repository/TenantRepository.java
@@ -3,10 +3,12 @@ package com.shakirali.rental.repository;
 import com.shakirali.rental.entity.Tenant;
 import com.shakirali.rental.entity.TenantId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TenantRepository extends JpaRepository<Tenant, TenantId> {
 
+    @Query(value = "SELECT * from Tenant WHERE name = :name And mobile_no = :mobileNo", nativeQuery = true)
     Tenant findByNameAndMobile_No(String name, String mobileNo);
 }
